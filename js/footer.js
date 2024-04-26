9aec940629dec5f1b6d85818b799a0559e658886
Luis4978 <luis4978@hotmail.com>
2024-04-26 19:52:07 -0300

    Fusion en rama luis_arevalo con rama main

---
+++ js/footer.js
@@ -0,0 +1,30 @@
+
+
+// document.getElementById("footer").innerHTML = `
+// <div class="footer_container">
+// <div class="footer_section">
+//     <h4>Logo icono</h4>
+// </div>
+// <div class="footer_section">
+//     <h4>Siguenos en</h4>
+//     <a href="https://www.twitter.com" target="_blank">
+//         <img class="img_icons" src="//imagenes/twitter_icon.svg" alt="link a twitter">
+//     </a>
+//     <a href="https://www.facebook.com/" target="_blank">
+//         <img class="img_icons" src="/imagenes/facebook_icon.svg" alt="link a facebook">
+//     </a>
+//     <a href="https://www.instagram.com/?hl=es" target="_blank">
+//         <img class="img_icons" src="/imagenes/instagram_icon.svg" alt="link a instagram">
+//     </a>
+//     <a href="https://www.linkedin.com/" target="_blank">
+//             <img class="img_icons" src="/imagenes/linkedin_icon.svg" alt="link a linkedin">
+//     </a>
+// </div>
+// <div class="footer_section">
+//     <p>&copy; 2024 MindTech. All Rights Reserved </p>
+// </div>
+// <a href="#" class="img_icons">
+//     <img src="/imagenes/flecha_home.svg" alt="ir arriba flecha">
+// </a>
+// </div>
+// `
