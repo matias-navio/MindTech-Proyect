eb2ee25ce76b9f4b920c2b7204c893f9fe1b2b63
Matias Navio <naviomatias2@gmail.com>
2024-05-23 11:02:23 -0300

    Arreglos en el footer y responsive footer

--- js/footer.js
+++ js/footer.js
@@ -1,30 +1,27 @@
-
-
-// document.getElementById("footer").innerHTML = `
-// <div class="footer_container">
-// <div class="footer_section">
-//     <h4>Logo icono</h4>
-// </div>
-// <div class="footer_section">
-//     <h4>Siguenos en</h4>
-//     <a href="https://www.twitter.com" target="_blank">
-//         <img class="img_icons" src="//imagenes/twitter_icon.svg" alt="link a twitter">
-//     </a>
-//     <a href="https://www.facebook.com/" target="_blank">
-//         <img class="img_icons" src="/imagenes/facebook_icon.svg" alt="link a facebook">
-//     </a>
-//     <a href="https://www.instagram.com/?hl=es" target="_blank">
-//         <img class="img_icons" src="/imagenes/instagram_icon.svg" alt="link a instagram">
-//     </a>
-//     <a href="https://www.linkedin.com/" target="_blank">
-//             <img class="img_icons" src="/imagenes/linkedin_icon.svg" alt="link a linkedin">
-//     </a>
-// </div>
-// <div class="footer_section">
-//     <p>&copy; 2024 MindTech. All Rights Reserved </p>
-// </div>
-// <a href="#" class="img_icons">
-//     <img src="/imagenes/flecha_home.svg" alt="ir arriba flecha">
-// </a>
-// </div>
-// `
+document.getElementById("footer").innerHTML = `
+    <div class="footer_container">
+        <div class="footer_section">
+            <img class="logo-footer" src="/assets/img/logoCapMindTech.png" alt="">
+        </div>
+        <div class="footer_section">
+            <h4 class="titulo_socials">Siguenos en:</h4>
+            <div class="icons-container">
+                <a href="https://www.twitter.com" target="_blank">
+                    <img class="img_icons" src="/assets/img/twitter_icon.svg" alt="link a twitter">
+                </a>
+                <a href="https://www.facebook.com/" target="_blank">
+                    <img class="img_icons" src="/assets/img/facebook_icon.svg" alt="link a facebook">
+                </a>
+                <a href="https://www.instagram.com/?hl=es" target="_blank">
+                    <img class="img_icons" src="/assets/img/instagram_icon.svg" alt="link a instagram">
+                </a>
+                <a href="https://www.linkedin.com/" target="_blank">
+                    <img class="img_icons" src="/assets/img/linkedin_icon.svg" alt="link a linkedin">
+                </a>
+            </div>
+        </div>
+        <div class="footer_section">
+            <p class="text_copia">&copy; 2024 MindTech. All Rights Reserved </p>
+        </div>
+    </div>
+`
