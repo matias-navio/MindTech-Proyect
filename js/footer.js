ca074e8298e884c20540b19db77a2304542faae7
Matias Navio <naviomatias2@gmail.com>
2024-05-23 11:59:51 -0300

    Archivos de footer

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.getElementById("idFooter").innerHTML = `
 <div class="footer_container">
 <div class="footer_section">
@@ -26,32 +25,5 @@ document.getElementById("idFooter").innerHTML = `
     <img src="/assets/img/flecha_home.svg" alt="ir arriba flecha">
 </a>
 </div>
-=======
-document.getElementById("footer").innerHTML = `
-    <div class="footer_container">
-        <div class="footer_section">
-            <img class="logo-footer" src="/assets/img/logoCapMindTech.png" alt="">
-        </div>
-        <div class="footer_section">
-            <h4 class="titulo_socials">Siguenos en:</h4>
-            <div class="icons-container">
-                <a href="https://www.twitter.com" target="_blank">
-                    <img class="img_icons" src="/assets/img/twitter_icon.svg" alt="link a twitter">
-                </a>
-                <a href="https://www.facebook.com/" target="_blank">
-                    <img class="img_icons" src="/assets/img/facebook_icon.svg" alt="link a facebook">
-                </a>
-                <a href="https://www.instagram.com/?hl=es" target="_blank">
-                    <img class="img_icons" src="/assets/img/instagram_icon.svg" alt="link a instagram">
-                </a>
-                <a href="https://www.linkedin.com/" target="_blank">
-                    <img class="img_icons" src="/assets/img/linkedin_icon.svg" alt="link a linkedin">
-                </a>
-            </div>
-        </div>
-        <div class="footer_section">
-            <p class="text_copia">&copy; 2024 MindTech. All Rights Reserved </p>
-        </div>
-    </div>
->>>>>>> 304f6383ed6b3e4605de999b6463cffb7fdc598b
+
 `
